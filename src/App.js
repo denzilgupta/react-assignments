41ceb0b035110d5c947d456b6271f328c09e1f3b
Denzil Gupta <denzil.gupta@gmail.com>
2020-12-24 15:13:24 +0530

    reset for assignment 2

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
 import React, { Component } from 'react';
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
 import './App.css';
 
 class App extends Component {
-  state = {
-    usernames: [
-      { name: "Sam" },
-      { name: "Rick" },
-      { name: "Morty" }
-    ]
-  };
-
-  nameChangeHandler = (event) => {
-    this.setState({
-      usernames: [
-        { name: event.target.value },
-        { name: "Rick" },
-        { name: "Morty" }
-      ]
-    });
-  };
-
   render() {
     return (
-      <div className="App" >
-        <UserInput
-          change={this.nameChangeHandler}
-          currentName={this.state.usernames[0].name}
-        ></UserInput>
-        <UserOutput
-          name={this.state.usernames[0].name}
-        />
-        <UserOutput
-          name={this.state.usernames[1].name}
-        />
-        <UserOutput
-          name={this.state.usernames[2].name}
-        />
-      </div>
-    );
+      <div className="App">Hi</div>
+    )
   }
 }
 
